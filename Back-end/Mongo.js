a75d5373874b5948462c34541a55a98c9b7d0bfd
Muhammad Ahmad Fridi <muhammadahmadfridi71al@gmail.com>
2025-09-24 10:15:34 +0500

    Initial commit

--- Back-end/Mongo.js
+++ Back-end/Mongo.js
@@ -1,34 +1,16 @@
-// import mongoose from "mongoose";
-
-// function connectToMongoDB() {
-//   mongoose
-//     .connect(
-//       "mongodb+srv://kanbanboard:1122334455@mypersonalcluster.mesbsq1.mongodb.net/todoApp?retryWrites=true&w=majority&appName=myPersonalCluster",
-//       {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//       }
-//     )
-//     .then(() => {
-//       console.log("MongoDB connected successfully!");
-//     })
-//     .catch((err) => {
-//       console.error("MongoDB connection failed:", err);
-//     });
-// }
-
-// export default connectToMongoDB;
-
-
-
-
 import mongoose from "mongoose";
 
 function connectToMongoDB() {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/todoApp") 
+    .connect(
+      "mongodb+srv://kanbanboard:1122334455@mypersonalcluster.mesbsq1.mongodb.net/todoApp?retryWrites=true&w=majority&appName=myPersonalCluster",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     .then(() => {
-      console.log("MongoDB connected successfully");
+      console.log("MongoDB connected successfully!");
     })
     .catch((err) => {
       console.error("MongoDB connection failed:", err);
@@ -36,3 +18,21 @@ function connectToMongoDB() {
 }
 
 export default connectToMongoDB;
+
+
+
+
+// import mongoose from "mongoose";
+
+// function connectToMongoDB() {
+//   mongoose
+//     .connect("mongodb://127.0.0.1:27017/todoApp") 
+//     .then(() => {
+//       console.log("MongoDB connected successfully");
+//     })
+//     .catch((err) => {
+//       console.error("MongoDB connection failed:", err);
+//     });
+// }
+
+// export default connectToMongoDB;
